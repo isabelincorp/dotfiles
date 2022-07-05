ed9330ebe782e45645201529c9162bb533fed6e0
Isabel <srikeo@tuta.io>
2022-07-05 08:57:46 -0700

    Create ElementCatppuccin1Theme.js

---
+++ .config/Element/ElementCatppuccin1Theme.js
@@ -0,0 +1,32 @@
+{
+    "name": "Old Catppuccin",
+    "is_dark": true,
+    "colors": {
+        "accent-color": "#747ff4",
+        "accent": "#747ff4",
+        "primary-color": "#00aff4",
+        "warning-color": "#faa81ad9",
+        "alert": "#faa81ad9",
+
+        "sidebar-color": "#202225",
+        "roomlist-background-color": "#2f3136",
+        "roomlist-text-color": "#dcddde",
+        "roomlist-text-secondary-color": "#8e9297",
+        "roomlist-highlights-color": "#4f545c52",
+        "roomlist-separator-color": "#40444b",
+
+        "timeline-background-color": "#36393f",
+        "timeline-text-color": "#dcddde",
+        "secondary-content": "#dcddde",
+        "tertiary-content": "#dcddde",
+        "timeline-text-secondary-color": "#b9bbbe",
+        "timeline-highlights-color": "#04040512",
+
+        "reaction-row-button-selected-bg-color": "#4752c4",
+        "menu-selected-color": "#4752c4",
+        "focus-bg-color": "#4752c4",
+        "room-highlight-color": "#4752c4",
+        "other-user-pill-bg-color": "#4752c4",
+        "togglesw-off-color": "#72767d"
+    }
+}
