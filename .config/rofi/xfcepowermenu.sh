58395bd758fc21d7a5090ca49b23f911091ba283
Isabel <srikeo@tuta.io>
2022-08-20 23:33:57 -0700

    added powermenus
    note for self for use: go into /etc/sudoers.d/deployer then put in
    %USER ALL= NOPASSWD: /bin/systemctl poweroff
    %USER ALL= NOPASSWD: /bin/systemctl reboot

---
+++ .config/rofi/xfcepowermenu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# options to be displayed
+option0="lock screen"
+option1="logout"
+option2="reboot"
+option3="poweroff"
+
+# options passed into variable
+options="$option0\n$option1\n$option2\n$option3"
+
+chosen="$(echo -e "$options" | rofi -lines 8 -dmenu -p "power")"
+case $chosen in
+    $option0)
+        i3lock -c 000000 --no-verify -u;;
+    $option1)
+        pkill xfce4-session;;
+    $option2)
+        systemctl reboot -i;;
+	$option3)
+        systemctl poweroff -i;;
+esac
